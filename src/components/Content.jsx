50202e5e1c99fed82ab20cc4b20219d07c140c45
PetrusAriaa <petrusariacr25@gmail.com>
2024-06-29 18:57:07 +0700

    style: fix mobile view

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,12 @@
 const MainContent = () => {
   return (
     <div>
-      <div className="bg-white min-h-96 w-full flex flex-col items-center justify-center gap-8 py-28">
+      <div className="bg-white min-h-96 w-full flex flex-col items-center justify-center gap-8 py-14 sm:py-28">
         <div className="text-center">
-          <h1 className="text-5xl">Jadwal Ekaristi</h1>
-          <p className="text-2xl">Gereja Santo Matias Rasul Kosambi Baru</p>
+          <h1 className="text-3xl sm:text-5xl">Jadwal Ekaristi</h1>
+          <p className="text-lg sm:text-2xl">Gereja Santo Matias Rasul Kosambi Baru</p>
         </div>
-        <div className="flex flex-row gap-24">
+        <div className="flex flex-row gap-10 sm:gap-24">
           <div className="text-center flex flex-col gap-2">
             <h2 className="text-xl font-semibold text-blue-primary">Misa Harian</h2>
             <div>
@@ -33,11 +33,11 @@ const MainContent = () => {
           </div>
         </div>
       </div>
-      <div className="bg-blue-secondary min-h-96 w-full flex justify-center">
-        <div className="w-3/4 max-w-[1280px] h-96 flex justify-center items-center gap-8">
+      <div className="bg-blue-secondary min-h-screen sm:min-h-96 py-28 sm:py-0 sm:h-fit w-full flex justify-center">
+        <div className="w-3/4 max-w-[1280px] h-screen sm:h-96 flex flex-col sm:flex-row justify-center items-center gap-8">
           <div>
             <img src="https://cdn.discordapp.com/attachments/1245367927130034199/1256572908252172378/keling_compress.png?ex=66814237&is=667ff0b7&hm=febeab07262d6378237c6a17587a8702bdd6916d618e344096f1e2d6a2c658eb&"
-            className="w-72 rounded-xl object-cover" />
+            className="w-64 sm:w-72 rounded-xl object-cover" />
           </div>
           {/* <div className="size-72 bg-slate-200 rounded-xl animate-pulse flex-shrink-0" /> */}
           <div className="flex flex-col gap-4">
