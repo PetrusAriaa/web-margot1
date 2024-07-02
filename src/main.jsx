c98bc5eeed2544c783f6c13dcf80a970c358b858
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-03 00:23:49 +0700

    feat: create page MariaGoretti

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import BaseApp from './BaseApp.jsx'
 import BaseView from './BaseView.jsx'
 import './index.css'
+import MariaGorettiPage from './pages/MariaGoretti.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <BaseView content={<BaseApp />} />,
   },
+  {
+    path: "/maria-goretti",
+    element: <BaseView content={<MariaGorettiPage />} />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
