348bcb8ecaa329bd96154debeda7bb00d5374765
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-04 02:27:15 +0700

    style: disable button

--- src/app/components/Photos.tsx
+++ src/app/components/Photos.tsx
@@ -8,29 +8,29 @@ const Photos = () => {
         <div className="flex flex-col w-full gap-4">
           <div className="flex flex-col sm:flex-row gap-4">
             <div className="w-full flex">
-              <Image isZoomed src="https://storage.googleapis.com/margot-web/tatib1.jpeg" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}}/>
+              <Image isZoomed src="https://storage.googleapis.com/margot-web/tatib1.jpeg" alt="" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}}/>
             </div>
             <div className="w-full">
-              <Image isZoomed src="https://storage.googleapis.com/margot-web/ziarek1_compressed.jpeg" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
+              <Image isZoomed src="https://storage.googleapis.com/margot-web/ziarek1_compressed.jpeg" alt="" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
             </div>
             <div className="w-full">
-              <Image isZoomed src="https://storage.googleapis.com/margot-web/ziarek2_compressed.jpeg" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
+              <Image isZoomed src="https://storage.googleapis.com/margot-web/ziarek2_compressed.jpeg" alt="" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
             </div>
           </div>
           <div className="flex gap-4">
             <div className="w-full">
-              <Image isZoomed src="https://storage.googleapis.com/margot-web/koor_1.jpeg" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
+              <Image isZoomed src="https://storage.googleapis.com/margot-web/koor_1.jpeg" alt="" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
             </div>
             <div className="w-full">
-              <Image isZoomed src="https://storage.googleapis.com/margot-web/hero_thumb_compressed.png" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
+              <Image isZoomed src="https://storage.googleapis.com/margot-web/hero_thumb_compressed.png" alt="" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}} />
             </div>
             <div className="w-full">
-              <Image isZoomed src="https://storage.googleapis.com/margot-web/koor_1.jpeg" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}}/>
+              <Image isZoomed src="https://storage.googleapis.com/margot-web/koor_1.jpeg" alt="" classNames={{wrapper: "min-w-full", img: "h-56 object-cover w-full"}}/>
             </div>
           </div>
         </div>
         <div>
-          <button disabled className="disabled:cursor-not-allowed border-2 border-blue-primary px-4 py-2 rounded-lg font-medium hover:bg-blue-primary hover:text-white transition-colors">Kunjungi Galeri</button>
+          <button disabled className="disabled:cursor-not-allowed disabled:border-0 disabled:bg-slate-300 disabled:text-slate-400 border-2 border-blue-primary px-4 py-2 rounded-lg font-medium hover:bg-blue-primary hover:text-white transition-colors">Kunjungi Galeri</button>
         </div>
       </div>
     </div>
