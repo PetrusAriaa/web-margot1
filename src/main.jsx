8197e31c532c136a4ef4e2a2259b745972648288
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-03 14:04:15 +0700

    fix: add nextui provider to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {NextUIProvider} from '@nextui-org/react'
 import BaseApp from './BaseApp.jsx'
 import BaseView from './BaseView.jsx'
 import './index.css'
@@ -23,6 +24,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
   </React.StrictMode>
 )
