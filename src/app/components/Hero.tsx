db369d83109617cf7272e7eb4ee905594571ce1e
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-04 03:21:54 +0700

    feat: add swiper to hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,8 +1,52 @@
+'use client'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css';
+import { Image } from '@nextui-org/react';
+
+
 const HeroSection = () => {
   return (
     <div className="w-full">
-      <img src="https://storage.googleapis.com/margot-web/hero_thumb_compressed.png"
-        className="object-cover h-48 sm:h-96 w-full" />
+        <Swiper slidesPerView={1}
+          loop
+          className='w-full h-48 sm:h-[30rem]'
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          centeredSlides
+        modules={[Autoplay]}
+        >
+          <SwiperSlide className='overflow-hidden'>
+            <Image
+              className='w-full'
+              alt='swiper-image'
+              radius='none'
+            src='https://storage.googleapis.com/margot-web/hero_thumb_compressed.png'/>
+          </SwiperSlide>
+          <SwiperSlide className='bg-red-500'>
+            <Image
+              className='w-full'
+              alt='swiper-image'
+              radius='none'
+            src='https://storage.googleapis.com/margot-web/ziarek_1.webp'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              className='w-full'
+              alt='swiper-image'
+              radius='none'
+            src='https://storage.googleapis.com/margot-web/ziarek_anyer.webp' />
+          </SwiperSlide>
+          <SwiperSlide className='flex w-full'>
+            <Image
+              className='min-w-full'
+              alt='swiper-image'
+              radius='none'
+            src='https://storage.googleapis.com/margot-web/ziarek_2022_resize.webp' />
+          </SwiperSlide>
+        </Swiper>
     </div>
   )
 }
