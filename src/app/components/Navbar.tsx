f376f856a7a13bee2f5ba1a5e4a68ee4eeaf6970
PetrusAriaa <petrusariacr25@gmail.com>
2024-07-09 13:12:53 +0700

    style: persist navbar display on mobile view

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -28,30 +28,30 @@ const handleScrollChange = (position: number) => {
       if (item.className.includes(" !text-blue-primary")) {
         return
       }
-      item.className = deleteClass(item.className, " text-neutral-white")
+      item.className = deleteClass(item.className, " text-blue-primary lg:text-neutral-white")
       item.className += " !text-blue-primary"
     })
     if (_navbar?.className.includes(" lg:!bg-neutral-white")) {
       return
     }
-    imageWrapper.className = deleteClass(imageWrapper.className, " w-16 h-16")
+    imageWrapper.className = deleteClass(imageWrapper.className, " lg:w-16 lg:h-16")
     _navbar.className = deleteClass(_navbar.className, " lg:!bg-transparent")
-    _navbar.className += " lg:!bg-neutral-white !py-0"
-    imageWrapper.className += " w-10 h-10"
+    _navbar.className += " lg:!bg-neutral-white lg:!py-0"
+    imageWrapper.className += " lg:w-10 lg:h-10"
     return 
   }
   else {
     navbarText.forEach((item) => {
       if (item.className.includes(" !text-blue-primary")) {
         item.className = deleteClass(item.className, " !text-blue-primary")
-        item.className += " text-neutral-white"
+        item.className += " text-blue-primary lg:text-neutral-white"
       }
     })
     if (_navbar?.className.includes(" lg:!bg-neutral-white")) {
-      _navbar.className = deleteClass(_navbar.className, " lg:!bg-neutral-white !py-0")
+      _navbar.className = deleteClass(_navbar.className, " lg:!bg-neutral-white lg:!py-0")
       _navbar.className += " lg:!bg-transparent"
-      imageWrapper.className = deleteClass(imageWrapper.className, " w-10 h-10")
-      imageWrapper.className += " w-16 h-16"
+      imageWrapper.className = deleteClass(imageWrapper.className, " lg:w-10 lg:h-10")
+      imageWrapper.className += " lg:w-16 lg:h-16"
     }
   }
 }
@@ -61,7 +61,7 @@ const MyNavbar = () => {
 
   return (
     <Navbar id="navbar" classNames={{
-      base: ["fixed", "bg-neutral-white", "lg:bg-transparent", "border-b", "border-neutral-white/25", "py-4", "transition-all", "shadow-md"]
+      base: ["fixed", "bg-neutral-white", "lg:bg-transparent", "border-b", "border-neutral-white/25", "py-0" ,"lg:py-4", "transition-all", "shadow-md"]
       }}
       disableScrollHandler={false}
       onScrollPositionChange={(yPos) => handleScrollChange(yPos)}
@@ -70,12 +70,12 @@ const MyNavbar = () => {
       onMenuOpenChange={()=>setMenuOpen(!isMenuOpen)}>
       <NavbarBrand as={Link} href="/">
         <div className="flex flex-shrink-0 flex-row items-center gap-2">
-          <div className="__navbar-logo-image__ relative w-16 h-16 scale-100 transition-all">
+          <div className="__navbar-logo-image__ relative w-10 h-10 lg:w-16 lg:h-16 transition-all">
             <Image src="/logo_200.png" alt="Logo" fill sizes="4rem" priority/>
           </div>
           <div>
-            <p id="logo-text1" className='text-sm __navbar-text__ text-neutral-white transition-colors'>Lingkungan</p>
-            <p id="logo-text2" className='text-base font-semibold text-neutral-white __navbar-text__ transition-colors'>Santa Maria Goretti</p>
+            <p id="logo-text1" className='text-sm __navbar-text__ text-blue-primary lg:text-neutral-white transition-colors'>Lingkungan</p>
+            <p id="logo-text2" className='text-base font-semibold text-blue-primary lg:text-neutral-white __navbar-text__ transition-colors'>Santa Maria Goretti</p>
           </div>
         </div>
       </NavbarBrand>
